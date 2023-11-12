be49314ca88822afbe357488541c5e859b3772bf
artium_96 <artium20@gmail.com>
2023-11-12 14:56:23 +0200

    fix bag RECAPTCHA

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -116,8 +116,8 @@ const Contact: React.FC = () => {
           
           {/* check box if user dont robot */}
           <ReCAPTCHA
-            className={ process.env.REACT_APP_RECAPTCHA || ""}
-            sitekey="6LfedwwpAAAAAPLsv3zMHa4cjZvaX0FY2W-o9OFj"
+            className="g-recaptcha"
+            sitekey={ process.env.REACT_APP_RECAPTCHA || "" } 
             onChange={(val) => setCapVal(val)}
           />
 
