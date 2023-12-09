e0fef3658077096c3fe887e351c4c108ba7d9e45
artium_96 <artium20@gmail.com>
2023-12-09 15:27:52 +0200

    add version to sanity , fix bag

--- src/Sanity/client.tsx
+++ src/Sanity/client.tsx
@@ -3,6 +3,10 @@ import { createClient } from "@sanity/client";
 const client = createClient({
   projectId: process.env.REACT_APP_SANITY || "",
   dataset: "production",
+  useCdn: true,
+  apiVersion: "2023-08-28"
 });
+
+
 
 export default client;
