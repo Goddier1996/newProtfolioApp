6f117f9f661ba93e937fcacdc2da819a3a5a92ab
artium_96 <artium20@gmail.com>
2024-01-04 17:52:02 +0200

    fix bag read  at card about me

--- src/components/aboutMe/infoAboutMe/modelsCardsAboutMe/CardsAboutMe.tsx
+++ src/components/aboutMe/infoAboutMe/modelsCardsAboutMe/CardsAboutMe.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { InfoAboutMe } from "../../../../interface/info.model";
 
 
+
 const CardsAboutMe: React.FC<InfoAboutMe> = (props) => {
 
 
@@ -11,26 +12,26 @@ const CardsAboutMe: React.FC<InfoAboutMe> = (props) => {
   const [showMore, setShowMore] = useState<Boolean>(false);
   const sizeWorldInDisc: number = info.length;
 
-  
   return (
+
     <div className="ContainerAboutMeCards">
       <span className="styleIconAboutMe">
         <Icon />
       </span>
       <h1>{title}</h1>
 
-      
+      <p>
         {sizeWorldInDisc < 100 ? (
-          <p>{info}</p>
+          <>{info}</>
         ) : (
           <>
-            {showMore ? <p>{info}</p> : <p>{info.substring(0, 50)}</p>}
+            {showMore ? info : `${info.substring(0, 70)}`}
             <b onClick={() => setShowMore(!showMore)}>
               {showMore ? "Show less" : "Read more"}
             </b>
           </>
         )}
-      
+      </p>
     </div>
   );
 };
