ab28cf7a5d04e334abd5e75cbc0b80da3ef13bf5
artium_96 <artium20@gmail.com>
2024-01-14 21:52:11 +0200

    change style menu

--- src/components/startPortfolio/ProfileStart.tsx
+++ src/components/startPortfolio/ProfileStart.tsx
@@ -11,9 +11,9 @@ const ProfileStart:React.FC = () => {
     <div className="ContainerProfileStart">
       <Slide direction="left">
         <div className="Texts">
-          <h4>
+          {/* <h4>
             Hi <span className="green">There</span> Welcome To The My Portfolio
-          </h4>
+          </h4> */}
           <h1 className="green">Artem Kot</h1>
           <h3>
             Software Developer Inclination towards Front End
