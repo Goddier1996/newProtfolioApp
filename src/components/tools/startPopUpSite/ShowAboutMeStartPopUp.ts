a7a6b0272d680b0d26f73a9ebfd62650df71d452
artium_96 <artium20@gmail.com>
2024-04-16 15:27:55 +0300

    change in start popup video about me

--- src/components/tools/startPopUpSite/ShowAboutMeStartPopUp.ts
+++ src/components/tools/startPopUpSite/ShowAboutMeStartPopUp.ts
@@ -11,10 +11,10 @@ export function showPopUpInfoApp() {
           <video
             class="videoShowPopUpAboutMe"
             controls
-            poster="https://i.postimg.cc/NMv6V0J5/12.webp"
+            poster="https://i.postimg.cc/4ymvK455/12.webp"
            >
             <source
-            src="https://github.com/Goddier1996/newProtfolioApp/assets/59862302/5783d19b-7fd8-48bf-b834-c3ef1daf0894"
+            src="https://github.com/Goddier1996/newProtfolioApp/assets/59862302/35a1a473-2795-4ed8-9ce6-2ec780fb70ab"
             type="video/mp4">
             </video>
         </div>`,
