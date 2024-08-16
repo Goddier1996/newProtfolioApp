0d61c3a17def2fc3e17536727d358ac3f08ae4d9
artium_96 <artium20@gmail.com>
2024-08-16 18:27:31 +0300

    change position start popup video about me,to bottom-end position

--- src/components/tools/startPopUpSite/ShowAboutMeStartPopUp.ts
+++ src/components/tools/startPopUpSite/ShowAboutMeStartPopUp.ts
@@ -7,7 +7,9 @@ export function showPopUpInfoApp() {
   sessionStorage.setItem("startPopUp", "userSeeStartPopUp");
 
   Swal.fire({
-    html: `<div class="showPopUpAboutMe">
+    html: `
+    <div class="showPopUpAboutMe">
+
           <video
             class="videoShowPopUpAboutMe"
             controls
@@ -17,11 +19,20 @@ export function showPopUpInfoApp() {
             src="https://github.com/Goddier1996/newProtfolioApp/assets/59862302/47c55fde-6e34-47c8-bef5-e58162283bdf"
             type="video/mp4">
             </video>
+
+          <div class="arrowShowPopUp">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
         </div>`,
     background: "none",
-    width: "800px",
+    width: "410px",
     showConfirmButton: false,
     showCloseButton: true,
+    allowOutsideClick: false,
+    position: "bottom-end"
     // timer: 52000
     // autoplay
     // muted
