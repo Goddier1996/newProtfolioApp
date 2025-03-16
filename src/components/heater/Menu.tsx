e6eee24f56b2d58d9557a702d7c8feb4494294f7
artium_96 <artium20@gmail.com>
2025-03-16 10:17:11 +0200

    remove free code line

--- src/components/heater/Menu.tsx
+++ src/components/heater/Menu.tsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import styled from "styled-components";
 import "./Menu.css";
 import { ObjectMenuOptions } from "../../interface/info.model";
 import LazyImage from "../tools/LazyLoadImage/LazyImage";
 
 
-
 const Header: React.FC = () => {
-
 
   const [bar, setBar] = useState<Boolean>(false);
 
@@ -19,40 +16,35 @@ const Header: React.FC = () => {
   ];
 
 
-
   return (
-      <div className="ContainerMenu">
-          <div className="Logo">
-            <LazyImage
-              src="https://i.postimg.cc/QN7XRWh6/qa.webp"
-              width={NaN}
-              height={50}
-              alt="Logo"
-            />
-          </div>
-
-        <div className="Nav" style={bar ? { height: "100vh" } : { height: 0 }}>
-          {arrayMenuOptions.map((value: ObjectMenuOptions) => (
-            <span key={value.id}>
-              <a
-                aria-label={value.type}
-                href={"#" + value.type}
-                onClick={() => {
-                  bar ? setBar(!bar) : setBar(bar);
-                }}
-              >
-                {value.type}
-              </a>
-            </span>
-          ))}
-        </div>
-
-       
+    <div className="ContainerMenu">
+      <div className="Logo">
+        <LazyImage
+          src="https://i.postimg.cc/QN7XRWh6/qa.webp"
+          width={NaN}
+          height={50}
+          alt="Logo"
+        />
       </div>
+
+      <div className="Nav" style={bar ? { height: "100vh" } : { height: 0 }}>
+        {arrayMenuOptions.map((value: ObjectMenuOptions) => (
+          <span key={value.id}>
+            <a
+              aria-label={value.type}
+              href={"#" + value.type}
+              onClick={() => {
+                bar ? setBar(!bar) : setBar(bar);
+              }}
+            >
+              {value.type}
+            </a>
+          </span>
+        ))}
+      </div>
+    </div>
   );
 };
 
 
 export default Header;
-
-
